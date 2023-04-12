33ababfa19c2deb8293ce2ad832af91be45e10c3
wonrax <hahuylonghai2012@gmail.com>
2023-04-12 14:46:46 +0700

    persistent volume on fly.io

--- internal/config.go
+++ internal/config.go
@@ -1,8 +1,10 @@
 package togo
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/joho/godotenv"
@@ -20,6 +22,7 @@ type ConfigType struct {
 	Environment   string
 	AppURL        string
 	AdminUsername string
+	DatabasePath  string
 }
 
 var Db *sqlx.DB
@@ -28,7 +31,7 @@ var basicAuthConfig *BasicAuthConfig
 var Config *ConfigType
 var Tables *TablesType
 
-func InitGlobalConfig(env string, d *sqlx.DB) {
+func InitGlobalConfig(env string) {
 	// Load environment variables
 	err := godotenv.Load()
 	if err != nil {
@@ -43,6 +46,7 @@ func InitGlobalConfig(env string, d *sqlx.DB) {
 	Config = &ConfigType{}
 	Config.Environment = env
 	Config.AppURL = os.Getenv("APP_URL")
+	Config.DatabasePath = os.Getenv("DATABASE_PATH")
 
 	err = initLogger(env)
 	if err != nil {
@@ -50,19 +54,83 @@ func InitGlobalConfig(env string, d *sqlx.DB) {
 	}
 
 	// Database related
-	initDb(d)
+	initDb()
 	Tables = &TablesType{
 		Users: "users",
 		Todos: "todos",
 	}
 
 	initBasicAuthConfig()
+
+	// Init admin user
+	adminUsername := os.Getenv("ADMIN_USERNAME")
+	adminPassword := os.Getenv("ADMIN_PASSWORD")
+	if adminUsername != "" && adminPassword != "" {
+		adminUser := &UserCredentials{
+			Username: &adminUsername,
+			Password: &adminPassword,
+		}
+		err = addAdminUser(adminUser)
+		if err != nil {
+			Log.Warn("Cannot update admin user", zap.Error(err))
+		} else {
+			Log.Info(fmt.Sprintf("Admin user `%s` updated", adminUsername))
+			Config.AdminUsername = adminUsername
+		}
+	} else {
+		Log.Warn("Admin user not configured")
+	}
 }
 
-func initDb(d *sqlx.DB) {
+func initDb() {
+	if Config.DatabasePath == "" {
+		Config.DatabasePath = "togo.db"
+		Log.Warn("Database path not set. Using default path: togo.db. The data could be lost every time the container is restarted. Set the DATABASE_PATH environment variable to a persistent path to avoid this.")
+	}
+
+	d, err := sqlx.Open("sqlite3", Config.DatabasePath)
+	if err != nil {
+		log.Fatal("Cannot open sqlite database", zap.Error(err))
+	}
+
 	Db = d
+
+	// DATABASE MIGRATION
+	DBMigrate()
 }
 
 func initBasicAuthConfig() {
 	basicAuthConfig = NewBasicAuthConfig()
+}
+
+func addAdminUser(u *UserCredentials) error {
+	adminUser := User{}
+	err := Db.Get(&adminUser, "SELECT * FROM users WHERE username = ?", u.Username)
+
+	key, salt := basicAuthConfig.HashPassword(*u.Password)
+	// generate iso 8601 timestamp for updated_at
+	timestamp := time.Now().UTC().Format(time.RFC3339)
+
+	if err != nil {
+		Log.Debug("Admin user does not exist. Creating one.")
+		_, err := Db.Exec(`
+			INSERT INTO users (username, hashed_password, password_salt, created_at, updated_at)
+			VALUES (?, ?, ?, ?, ?)
+		`, u.Username, key, salt, timestamp, timestamp)
+
+		if err != nil {
+			return err
+		}
+	} else {
+		// If exists, update the password
+		Log.Debug("Admin user exists. Updating password.")
+		_, err := Db.Exec(`
+			UPDATE users SET hashed_password = ?, password_salt = ?, updated_at = ?
+			WHERE username = ?
+		`, key, salt, timestamp, u.Username)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
--- internal/start.go
+++ internal/start.go
@@ -1,47 +1,17 @@
 package togo
 
 import (
-	"fmt"
-	"log"
 	"net/http"
-	"os"
-	"time"
 
-	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
-	"go.uber.org/zap"
 )
 
+// Seperate starting the server and setting up the app
+// because we want to be able to test the app without
+// starting the server
+
 func Setup(environment string) {
-	// DATABASE INITIALIZATION
-	db, err := sqlx.Open("sqlite3", "togo.db")
-	if err != nil {
-		log.Fatal("Cannot open sqlite database", zap.Error(err))
-	}
-
-	InitGlobalConfig(environment, db)
-
-	// DATABASE MIGRATION
-	DBMigrate()
-
-	// Init admin user
-	adminUsername := os.Getenv("ADMIN_USERNAME")
-	adminPassword := os.Getenv("ADMIN_PASSWORD")
-	if adminUsername != "" && adminPassword != "" {
-		adminUser := &UserCredentials{
-			Username: &adminUsername,
-			Password: &adminPassword,
-		}
-		err = addAdminUser(adminUser)
-		if err != nil {
-			Log.Warn("Cannot update admin user", zap.Error(err))
-		} else {
-			Log.Info(fmt.Sprintf("Admin user `%s` updated", adminUsername))
-			Config.AdminUsername = adminUsername
-		}
-	} else {
-		Log.Warn("Admin user not configured")
-	}
+	InitGlobalConfig(environment)
 }
 
 func Cleanup() {
@@ -57,36 +27,4 @@ func Start(environment string) {
 	port := "3000"
 	Log.Info("Starting server on port " + port)
 	http.ListenAndServe(":"+port, CreateRouter())
-}
-
-func addAdminUser(u *UserCredentials) error {
-	adminUser := User{}
-	err := Db.Get(&adminUser, "SELECT * FROM users WHERE username = ?", u.Username)
-
-	key, salt := basicAuthConfig.HashPassword(*u.Password)
-	// generate iso 8601 timestamp for updated_at
-	timestamp := time.Now().UTC().Format(time.RFC3339)
-
-	if err != nil {
-		Log.Debug("Admin user does not exist. Creating one.")
-		_, err := Db.Exec(`
-			INSERT INTO users (username, hashed_password, password_salt, created_at, updated_at)
-			VALUES (?, ?, ?, ?, ?)
-		`, u.Username, key, salt, timestamp, timestamp)
-
-		if err != nil {
-			return err
-		}
-	} else {
-		// If exists, update the password
-		Log.Debug("Admin user exists. Updating password.")
-		_, err := Db.Exec(`
-			UPDATE users SET hashed_password = ?, password_salt = ?, updated_at = ?
-			WHERE username = ?
-		`, key, salt, timestamp, u.Username)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
 }
