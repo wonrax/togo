05b8186ac9386c393cea85ec442e2c2255e0539b
wonrax <hahuylonghai2012@gmail.com>
2023-04-12 17:29:47 +0700

    update

--- www/pages/admin/index.tsx
+++ www/pages/admin/index.tsx
@@ -30,7 +30,8 @@ export default function AdminPage() {
   return (
     <Layout>
       <div className="min-w-full max-w-[100vw] min-h-[100vh] flex flex-col items-center px-6 py-16">
-        <div className="w-full sm:w-[800px] flex flex-col gap-8">
+        <div className="w-full sm:w-[800px] flex flex-col gap-4">
+          <p className="text-xl md:text-2xl font-bold">Users</p>
           {!error && !isLoading && <Data data={response?.data} />}
         </div>
       </div>
@@ -63,39 +64,52 @@ function Data({ data }) {
   })
 
   return (
-    <table className="border-collapse w-full  bg-white dark:bg-slate-800 text-sm">
+    <table className="border-collapse w-full dark:bg-slate-800 text-sm">
       <thead>
         {table.getHeaderGroups().map((headerGroup) => (
-          <tr key={headerGroup.id}>
-            {headerGroup.headers.map((header) => (
-              <th
-                key={header.id}
-                className="w-1/2 border-b  border-slate-300 dark:border-slate-600 font-semibold p-4 text-slate-900 dark:text-slate-200 text-left"
-              >
-                {header.isPlaceholder
-                  ? null
-                  : flexRender(
-                      header.column.columnDef.header,
-                      header.getContext()
-                    )}
-              </th>
-            ))}
+          <tr className="bg-gray-100" key={headerGroup.id}>
+            {headerGroup.headers.map((header, index) => {
+              let borderRadius = "border-y"
+              if (index === 0) borderRadius = " rounded-l border-l border-y"
+              else if (index === headerGroup.headers.length - 1)
+                borderRadius = " rounded-r border-r border-y"
+              return (
+                <th
+                  key={header.id}
+                  className={
+                    "w-1/2 font-semibold p-4 text-slate-900 dark:text-slate-200 text-left border-separate" +
+                    borderRadius
+                  }
+                >
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </th>
+              )
+            })}
           </tr>
         ))}
       </thead>
       <tbody>
-        {table.getRowModel().rows.map((row) => (
-          <tr key={row.id}>
-            {row.getVisibleCells().map((cell) => (
-              <td
-                key={cell.id}
-                className="border-t border-slate-300 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400"
-              >
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </td>
-            ))}
-          </tr>
-        ))}
+        {table.getRowModel().rows.map((row, index) => {
+          const border =
+            index === 0 ? "" : "border-t border-slate-300 dark:border-slate-700"
+          return (
+            <tr className={border} key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td
+                  key={cell.id}
+                  className="p-4 text-slate-500 dark:text-slate-400"
+                >
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          )
+        })}
       </tbody>
     </table>
   )
