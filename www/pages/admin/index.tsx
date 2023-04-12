49a32ded66280d277ad8aaf361f8d73c5f59e909
wonrax <hahuylonghai2012@gmail.com>
2023-04-12 16:55:41 +0700

    react table

--- www/pages/admin/index.tsx
+++ www/pages/admin/index.tsx
@@ -1,7 +1,13 @@
-import { FormEvent } from "react"
+import { useMemo, useReducer, useState } from "react"
 import { useRouter } from "next/router"
 import AppConfig from "@/common/config"
 import { fetcher } from "@/common/fetcher"
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table"
 import useSWR from "swr"
 
 import { Layout } from "@/components/layout"
@@ -12,7 +18,6 @@ export default function AdminPage() {
     data: response,
     error,
     isLoading,
-    mutate,
   } = useSWR(
     `${AppConfig.API_URL}/admin/users`,
     (url: string) => fetcher(url, router, true),
@@ -25,10 +30,73 @@ export default function AdminPage() {
   return (
     <Layout>
       <div className="min-w-full max-w-[100vw] min-h-[100vh] flex flex-col items-center px-6 py-16">
-        <div className="w-full sm:w-[400px] flex flex-col gap-8">
-          {JSON.stringify(response)}
+        <div className="w-full sm:w-[800px] flex flex-col gap-8">
+          {!error && !isLoading && <Data data={response?.data} />}
         </div>
       </div>
     </Layout>
+  )
+}
+type User = {
+  username: string
+  created_at: string
+}
+
+const columnHelper = createColumnHelper<User>()
+
+const columns = [
+  columnHelper.accessor("username", {
+    header: "Username",
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor("created_at", {
+    header: () => "Created at",
+    cell: (info) => new Date(info.getValue()).toLocaleDateString("en-UK"),
+  }),
+]
+
+function Data({ data }) {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  })
+
+  return (
+    <table className="border-collapse w-full  bg-white dark:bg-slate-800 text-sm">
+      <thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <th
+                key={header.id}
+                className="w-1/2 border-b  border-slate-300 dark:border-slate-600 font-semibold p-4 text-slate-900 dark:text-slate-200 text-left"
+              >
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map((row) => (
+          <tr key={row.id}>
+            {row.getVisibleCells().map((cell) => (
+              <td
+                key={cell.id}
+                className="border-t border-slate-300 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400"
+              >
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
   )
 }
